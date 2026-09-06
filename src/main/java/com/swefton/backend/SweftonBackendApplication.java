1315fb703a9e0384ecf87a91a473c0cf5b5635ee
FrankoKaloshi1 <fkaloshi22@epoka.edu.al>
2026-09-06 15:41:37 +0200

    SWF-4 Bootstrap Spring Boot backend

---
+++ src/main/java/com/swefton/backend/SweftonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swefton.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweftonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweftonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swefton/backend/SweftonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swefton.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweftonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
